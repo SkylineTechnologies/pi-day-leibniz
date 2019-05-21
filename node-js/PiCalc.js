b5d7496385e504afd8ccb0d80e37df1263ae04df
James Hughes <jhughes@epcor.com>
2019-05-21 12:57:09 -0600

    updating to use standard Math and Date libraries

--- node-js/PiCalc.js
+++ node-js/PiCalc.js
@@ -1,35 +1,30 @@
-"use strict"
+"use strict";
 
-const math = require ('mathjs');
-const moment = require ('moment');
 calcPi();
 
 function calcPi() {
-    let iterations = 0;
-    let startTime = new moment();
-    determinePi();
-    let endTime = new moment();
-    let miliSecs =0;
-    miliSecs = endTime.valueOf()-startTime.valueOf();
-    console.log(endTime.diff(startTime, 'miliseconds')/1000);
+  let startTime = new Date();
+  determinePi();
+  let endTime = new Date();
+  console.log((endTime - startTime) / 1000);
 }
 
 function determinePi() {
-    const realPi = 3.141592653589793;
-    const epsilon = 0.00000001;
-    let delta = 10;
-    let totalIterations =0;
-    let k=1;
+  const realPi = 3.141592653589793;
+  const epsilon = 0.00000001;
+  let delta = 10;
+  let totalIterations = 0;
+  let k = 1;
 
-    let pi_value =0.0;
+  let pi_value = 0.0;
 
-    while (delta > epsilon) {
-        pi_value += math.pow(-1, (k + 1)) / (2 * k - 1);
-        delta = math.abs((4 * pi_value) - realPi);
-        k++;
-    }
-    totalIterations = k;
-    pi_value = 4 * pi_value;
-    console.log("Total Iterations: " +  totalIterations);
-    console.log("Pi: " + pi_value);
+  while (delta > epsilon) {
+    pi_value += Math.pow(-1, k + 1) / (2 * k - 1);
+    delta = Math.abs(4 * pi_value - realPi);
+    k++;
+  }
+  totalIterations = k;
+  pi_value = 4 * pi_value;
+  console.log("Total Iterations: " + totalIterations);
+  console.log("Pi: " + pi_value);
 }
